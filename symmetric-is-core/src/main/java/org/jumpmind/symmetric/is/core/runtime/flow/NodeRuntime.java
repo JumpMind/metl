622ed9d31acd679ade7230abc6ea2a2b8a58223a
gwilmer <gwilmer@jumpmind.com>
2014-12-15 13:55:50 -0500

    Merge testing

--- symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/flow/NodeRuntime.java
+++ symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/flow/NodeRuntime.java
@@ -79,7 +79,7 @@ public class NodeRuntime implements Runnable {
         		}
         	}        	
         } catch (Exception ex) {
-        	//TODO: notify te flowruntime that we have an error and let it gracefully shut things down
+        	//TODO: notify the flowruntime that we have an error and let it gracefully shut things down
             error = ex;
         }
     }
