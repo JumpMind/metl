c3ad17a9f1ce57454bb13c8a5009305975902031
gwilmer <gwilmer@jumpmind.com>
2015-02-26 17:12:55 -0500

    dev checkin

---
+++ symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/FileReaderComponent.java
@@ -0,0 +1,168 @@
+package org.jumpmind.symmetric.is.core.runtime.component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import org.jumpmind.exception.IoException;
+import org.jumpmind.properties.TypedProperties;
+import org.jumpmind.symmetric.is.core.config.SettingDefinition;
+import org.jumpmind.symmetric.is.core.config.SettingDefinition.Type;
+import org.jumpmind.symmetric.is.core.runtime.IExecutionTracker;
+import org.jumpmind.symmetric.is.core.runtime.Message;
+import org.jumpmind.symmetric.is.core.runtime.connection.IConnectionFactory;
+import org.jumpmind.symmetric.is.core.runtime.connection.localfile.IStreamableConnection;
+import org.jumpmind.symmetric.is.core.runtime.flow.IMessageTarget;
+
+public class FileReaderComponent extends AbstractComponent {
+	
+	public static final String TYPE = "File Reader";
+	
+	private static final String FILE_TYPE_TEXT = "TEXT";
+	private static final String FILE_TYPE_BINARY = "BINARY";
+    public static final String DEFAULT_CHARSET = Charset.defaultCharset().name();
+
+	@SettingDefinition(order = 10, required = true, type = Type.STRING, label = "Path and File")
+	public final static String FILEREADER_RELATIVE_PATH = "filereader.relative.path";
+
+	@SettingDefinition(order = 20, type = Type.CHOICE, choices = {
+			FILE_TYPE_TEXT, FILE_TYPE_BINARY }, defaultValue = FILE_TYPE_TEXT, label = "File Type")
+	public final static String FILEREADER_FILE_TYPE = "filereader.file.type";
+
+	@SettingDefinition(type = Type.BOOLEAN, order = 30, required = true, provided = true, 
+			defaultValue = "true", label = "Must Exist")
+	public static final String FILEREADER_MUST_EXIST = "filereader.must.exist";
+
+	@SettingDefinition(type = Type.INTEGER, order = 40, required = true, provided = true, 
+			defaultValue = "1", label = "Binary Size / Msg (KB)")
+	public static final String FILEREADER_BINARY_SIZE_PER_MESSAGE = "filereader.binary.size.per.message";
+
+	@SettingDefinition(type = Type.INTEGER, order = 50, required = true, provided = true, 
+			defaultValue = "1", label = "Text Rows / Msg")
+	public static final String FILEREADER_TEXT_ROWS_PER_MESSAGE = "filereader.text.rows.per.message";
+	
+	@SettingDefinition(type = Type.INTEGER, order = 70, label = "Line Terminator")
+	public static final String FILEREADER_TEXT_HEADER_LINES_TO_SKIP = "filereader.text.header.lines.to.skip";
+
+	/* settings */
+	String relativePathAndFile;
+	String fileType;
+	boolean mustExist;
+	int binarySizePerMessage;
+	int textRowsPerMessage;
+	int textHeaderLinesToSkip;
+	
+	/* other vars */
+	TypedProperties properties;
+	InputStream inStream = null;
+	BufferedReader reader = null;
+    String encoding = DEFAULT_CHARSET;
+
+	@Override
+	public void start(IExecutionTracker executionTracker,
+			IConnectionFactory connectionFactory) {
+		super.start(executionTracker, connectionFactory);
+		appySettings();
+	}
+
+	@Override
+	public void handle(Message inputMessage, IMessageTarget messageTarget) {
+		if (fileType.equalsIgnoreCase(FILE_TYPE_BINARY)) {
+			handleBinaryFile(inputMessage, messageTarget);
+		} else {
+			handleTextFile(inputMessage, messageTarget);
+		}
+	}
+	
+	private void appySettings() {
+		properties = componentNode.getComponentVersion().toTypedProperties(
+				this, false);
+		relativePathAndFile = properties.get(FILEREADER_RELATIVE_PATH);
+		fileType = properties.get(FILEREADER_FILE_TYPE);
+		mustExist = properties.is(FILEREADER_MUST_EXIST);
+		binarySizePerMessage = properties.getInt(FILEREADER_BINARY_SIZE_PER_MESSAGE);
+		textRowsPerMessage = properties.getInt(FILEREADER_TEXT_ROWS_PER_MESSAGE);
+		textHeaderLinesToSkip = properties.getInt(FILEREADER_TEXT_HEADER_LINES_TO_SKIP);
+	}
+	
+	private void handleBinaryFile(Message inputMessage, IMessageTarget messageTarget) {
+		Message message = null;
+		ByteBuffer buffer = ByteBuffer.allocate(binarySizePerMessage*1024);
+		open();
+		try {
+			while (inStream.read(buffer.array()) != -1) {
+				message = new Message(componentNode.getId());
+				message.setPayload(new byte[buffer.remaining()]);
+				messageTarget.put(message);
+				buffer.clear();
+			}
+		} catch (IOException e) {
+			throw new IoException("Error reading from file "
+					+ e.getMessage());
+		} finally {
+			close();
+		}
+	}
+	
+	private void handleTextFile(Message inputMessage, IMessageTarget messageTarget) {
+		String currentLine;
+		int linesRead=0;
+		int linesInMessage=0;
+		open();
+		try {
+	        reader = new BufferedReader(new InputStreamReader(inStream, encoding));
+	        ArrayList<String> payload = new ArrayList<String>();
+	        while ((currentLine = reader.readLine()) != null) {
+	        	linesRead++;
+	        	if (linesRead > textHeaderLinesToSkip) {
+	        		if (linesInMessage >= textRowsPerMessage) {
+	        			initAndSendMessage(payload, messageTarget, linesInMessage);	        			
+	        		}
+	        		payload.add(currentLine);
+	        		linesInMessage++;
+	        	}
+	        }
+	        if (linesInMessage > 0) {
+	        	initAndSendMessage(payload, messageTarget, linesInMessage);
+	        }    
+		} catch (IOException e) {
+			throw new IoException("Error reading from file "
+					+ e.getMessage());			
+		} finally {
+			close();
+		}
+	}
+	
+	private void initAndSendMessage(ArrayList<String> payload, IMessageTarget messageTarget, int linesInMessage) {
+        Message message = new Message(componentNode.getId());
+        message.setPayload(payload);
+        messageTarget.put(message);
+        payload.clear();
+        linesInMessage = 0;
+	}
+	
+	private void open() {
+		IStreamableConnection connection = (IStreamableConnection) this.connection
+				.reference();
+		connection.appendPath(relativePathAndFile, mustExist);
+		inStream = connection.getInputStream();
+	}
+	
+	protected void close() {
+		try {
+			if (inStream != null) {
+				inStream.close();
+			}
+			if (reader != null) {
+				reader.close();
+			}
+		} catch (IOException e) {
+			throw new IoException("Failure in closing the reader "
+					+ e.getMessage());
+		}
+	}
+}
--- symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/FileWriterComponent.java
+++ symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/FileWriterComponent.java
@@ -1,10 +1,12 @@
 package org.jumpmind.symmetric.is.core.runtime.component;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 import org.jumpmind.exception.IoException;
 import org.jumpmind.properties.TypedProperties;
@@ -19,40 +21,62 @@ import org.jumpmind.symmetric.is.core.runtime.flow.IMessageTarget;
 public class FileWriterComponent extends AbstractComponent {
 
 	public static final String TYPE = "File Writer";
+	
+	private static final String FILE_TYPE_TEXT = "TEXT";
+	private static final String FILE_TYPE_BINARY = "BINARY";
 	private static final String DEFAULT_CHARSET = "UTF-8";
-
+	
 	@SettingDefinition(order = 10, required = true, type = Type.STRING, label = "Path and File")
 	public final static String FILEWRITER_RELATIVE_PATH = "filewriter.relative.path";
+	
+	@SettingDefinition(order = 20, type = Type.CHOICE, choices = {
+			FILE_TYPE_TEXT, FILE_TYPE_BINARY }, defaultValue = FILE_TYPE_TEXT, label = "File Type")
+	public final static String FILEWRITER_FILE_TYPE = "filereader.file.type";
 
-	@SettingDefinition(type = Type.BOOLEAN, order = 20, required = true, provided = true, defaultValue = "false", label = "Must Exist")
+	@SettingDefinition(type = Type.BOOLEAN, order = 30, required = true, provided = true, defaultValue = "false", label = "Must Exist")
 	public static final String FILEWRITER_MUST_EXIST = "filewriter.must.exist";
 
-	@SettingDefinition(type = Type.BOOLEAN, order = 30, required = true, provided = true, defaultValue = "false", label = "Append")
+	@SettingDefinition(type = Type.BOOLEAN, order = 40, required = true, provided = true, defaultValue = "false", label = "Append")
 	public static final String FILEWRITER_APPEND = "filewriter.append";
+	
+	@SettingDefinition(type = Type.INTEGER, order = 50, label = "Line Terminator")
+	public static final String FILEREADER_TEXT_LINE_TERMINATOR = "filereader.text.line.terminator";
 
+	/* settings */
+	String relativePathAndFile;
+	String fileType;
+	boolean mustExist;
+	boolean append;
+	String lineTerminator;
+
+	/* other vars */
 	TypedProperties properties;
 	OutputStream outStream;
-	BufferedWriter bufferedWriter;
+	BufferedWriter bufferedWriter=null;
 
 	@Override
 	public void start(IExecutionTracker executionTracker,
 			IConnectionFactory connectionFactory) {
 		super.start(executionTracker, connectionFactory);
-		properties = componentNode.getComponentVersion().toTypedProperties(
-				this, false);
+		applySettings();
 		outStream = getOutputStream((IStreamableConnection) this.connection
 				.reference());
-		bufferedWriter = initializeWriter(outStream);
+		if (fileType.equalsIgnoreCase(FILE_TYPE_TEXT)) {
+			bufferedWriter = initializeWriter(outStream);
+		}
 	}
 
 	@Override
 	public void handle(Message inputMessage, IMessageTarget messageTarget) {
-		try{
-			bufferedWriter.write((inputMessage.getPayload().toString()));
-			bufferedWriter.flush();
-		} catch (IOException e) {
-			throw new IoException("Error writing to file " + e.getMessage());
-		}
+		
+		/* we should get either an ArrayList of <String> for text file
+		 * or byte[] for a binary file
+		 */
+		if (inputMessage.getPayload() instanceof byte[]) {
+			handleBinaryFile(inputMessage, messageTarget);
+		} else {
+			handleTextFile(inputMessage, messageTarget);
+		}		
 	}
 
 	@Override
@@ -60,25 +84,58 @@ public class FileWriterComponent extends AbstractComponent {
 		close();
 		super.stop();
 	}
+		
+	private void applySettings() {
+		properties = componentNode.getComponentVersion().toTypedProperties(
+				this, false);
+		relativePathAndFile = properties.get(FILEWRITER_RELATIVE_PATH);
+		fileType = properties.get(FILEWRITER_FILE_TYPE);
+		mustExist = properties.is(FILEWRITER_MUST_EXIST);
+		append = properties.is(FILEWRITER_APPEND);
+		lineTerminator = properties.get(FILEREADER_TEXT_LINE_TERMINATOR);
+	}
+
+	private void handleBinaryFile(Message inputMessage, IMessageTarget messageTarget) {
+		if (fileType.equalsIgnoreCase(FILE_TYPE_TEXT)) {
+			throw new IoException("Converting from Binary input to Text output not implemented");
+		}
+	}
 	
-	protected OutputStream getOutputStream(IStreamableConnection conn) {
-		conn.appendPath(this.properties.get(FILEWRITER_RELATIVE_PATH),
-				this.properties.is(FILEWRITER_MUST_EXIST));
+	private void handleTextFile(Message inputMessage,
+			IMessageTarget messageTarget) {
+		if (fileType.equalsIgnoreCase(FILE_TYPE_BINARY)) {
+			throw new IoException(
+					"Converting from Text input to Binary output not implemented");
+		}
+		ArrayList<String> recs = inputMessage.getPayload();
+		try {
+			for (String rec : recs) {
+				bufferedWriter.write(rec);
+			}
+			bufferedWriter.flush();
+		} catch (IOException e) {
+			throw new IoException("Error writing to file " + e.getMessage());
+		}
+	}
+	
+	private OutputStream getOutputStream(IStreamableConnection conn) {
+		conn.appendPath(relativePathAndFile,
+				mustExist);
 		return conn.getOutputStream();
 	}
 
-	protected BufferedWriter initializeWriter(OutputStream stream) {
+	private BufferedWriter initializeWriter(OutputStream stream) {
 		try {
 			bufferedWriter = new BufferedWriter(new OutputStreamWriter(
 					outStream, DEFAULT_CHARSET));
 		} catch (UnsupportedEncodingException e) {
-			throw new IoException("Error setting default encoding "
+			throw new IoException("Error creating buffered writer "
 					+ e.getMessage());
 		}
 		return bufferedWriter;
 	}
 
-	protected void close() {
+	private void close() {
 		try {
 			if (bufferedWriter != null) {
 				bufferedWriter.close();
@@ -91,7 +148,7 @@ public class FileWriterComponent extends AbstractComponent {
 		}
 	}
 
-	protected void closeStream() {
+	private void closeStream() {
 		try {
 			if (outStream != null) {
 				outStream.close();
@@ -101,5 +158,4 @@ public class FileWriterComponent extends AbstractComponent {
 					+ e.getMessage());
 		}
 	}
-
 }
