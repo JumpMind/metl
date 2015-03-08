03fd8555ec17ad522b61c700d8a4160e36c1eb92
gwilmer <gwilmer@jumpmind.com>
2015-03-08 17:28:56 -0400

    Refactor Formatter to individual formatter types.  Rename DbComponentReader test to match component name.

--- symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/DelimitedFormatter.java
+++ symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/DelimitedFormatter.java
@@ -11,28 +11,18 @@ import org.jumpmind.symmetric.is.core.runtime.Message;
 import org.jumpmind.symmetric.is.core.runtime.flow.IMessageTarget;
 import org.jumpmind.symmetric.is.core.runtime.resource.IResourceFactory;
 
-@ComponentDefinition(typeName = Formatter.TYPE, category = ComponentCategory.PROCESSOR,
+@ComponentDefinition(typeName = DelimitedFormatter.TYPE, category = ComponentCategory.PROCESSOR,
 supports = { ComponentSupports.INPUT_MESSAGE, ComponentSupports.INPUT_MODEL, ComponentSupports.OUTPUT_MESSAGE })
 
-public class Formatter extends AbstractComponent {
+public class DelimitedFormatter extends AbstractComponent {
 
-    public static final String TYPE = "Formatter";
+    public static final String TYPE = "Delimited Formatter";
 
-    @SettingDefinition(order = 10, type = Type.CHOICE,
-            choices = { FormatType.DELIMITED, FormatType.FIXED_LENGTH, FormatType.XML }, 
-            label = "Format Type")
-    public final static String FORMATTER_FORMAT_TYPE = "formatter.format.type";
-    
-    @SettingDefinition(order = 20, required = true, type = Type.STRING, label = "Delimiter")
+    @SettingDefinition(order = 10, required = true, type = Type.STRING, label = "Delimiter")
     public final static String FORMATTER_DELIMITER = "formatter.delimiter";
     
-    @SettingDefinition(order = 30, required = true, type = Type.STRING, label = "Quote Character")
+    @SettingDefinition(order = 20, required = true, type = Type.STRING, label = "Quote Character")
     public final static String FORMATTER_QUOTE_CHARACTER = "formatter.quote.character";
-    
-    @SettingDefinition(type = Type.INTEGER, order = 40, defaultValue = "1",
-            label = "Text Rows / Msg")
-    public static final String FORMATTER_ROWS_PER_MESSAGE = "formatter.rows.per.message";
-
     
     /* settings */
     String formatType;
@@ -53,16 +43,12 @@ public class Formatter extends AbstractComponent {
         
         ArrayList<EntityData> records = inputMessage.getPayload();
         for (EntityData record:records) {
-            switch(formatType) {
-                case FormatType.DELIMITED: 
-            }
+            
         }
-        
     }
     
     private void applySettings() {
         properties = flowStep.getComponentVersion().toTypedProperties(this, false);
-        formatType = properties.get(FORMATTER_FORMAT_TYPE);
         delimiter = properties.get(FORMATTER_DELIMITER);
         quoteCharacter = properties.get(FORMATTER_QUOTE_CHARACTER);
     }
