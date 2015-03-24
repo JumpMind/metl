5900b7f443bddd9c1995862ec9f38544bcb9169e
Chris Henson <chenson42@gmail.com>
2015-03-24 12:28:10 -0400

    hack attack

--- symmetric-is-ui/src/main/java/org/jumpmind/symmetric/is/ui/views/design/EditFormatPanel.java
+++ symmetric-is-ui/src/main/java/org/jumpmind/symmetric/is/ui/views/design/EditFormatPanel.java
@@ -1,5 +1,8 @@
 package org.jumpmind.symmetric.is.ui.views.design;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jumpmind.symmetric.is.core.model.Component;
 import org.jumpmind.symmetric.is.core.model.ModelAttribute;
 import org.jumpmind.symmetric.is.core.model.ModelEntity;
@@ -8,6 +11,7 @@ import org.jumpmind.symmetric.is.ui.common.ButtonBar;
 import org.jumpmind.symmetric.ui.common.IUiPanel;
 
 import com.vaadin.data.Container;
+import com.vaadin.data.Item;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.BeanItemContainer;
@@ -35,238 +39,246 @@ import com.vaadin.ui.VerticalLayout;
 @SuppressWarnings("serial")
 public class EditFormatPanel extends VerticalLayout implements IUiPanel {
 
-	ApplicationContext context;
-	
-	Component component;
-	
-	Table table = new Table();
-	
-	BeanItemContainer<AttributeFormat> container = new BeanItemContainer<AttributeFormat>(AttributeFormat.class);
+    ApplicationContext context;
 
-	public EditFormatPanel(ApplicationContext context, Component component) {
-		this.context = context;
-		this.component = component;
+    Component component;
 
-		ButtonBar buttonBar = new ButtonBar();
-		addComponent(buttonBar);
+    Table table = new Table();
 
-		Button moveUpButton = buttonBar.addButton("Move Up", FontAwesome.ARROW_UP);
-		moveUpButton.addClickListener(new MoveUpClickListener());
-		
-		Button moveDownButton = buttonBar.addButton("Move Down", FontAwesome.ARROW_DOWN);
-		moveDownButton.addClickListener(new MoveDownClickListener());
+    BeanItemContainer<AttributeFormat> container = new BeanItemContainer<AttributeFormat>(
+            AttributeFormat.class);
 
-		table.setContainerDataSource(container);
-		table.setSelectable(true);
-		table.setSortEnabled(false);
-		table.setImmediate(true);
-		table.setSizeFull();
-		table.setVisibleColumns(new Object[] { "entityName", "attributeName", "width", "startPos", "endPos", "transformText" });
-		table.setColumnHeaders(new String[] { "Entity Name", "Attribute Name", "Width", "Start Position", "End Position", "Transform" });
-        table.setTableFieldFactory(new EditFieldFactory());        
+    public EditFormatPanel(ApplicationContext context, Component component) {
+        this.context = context;
+        this.component = component;
+
+        ButtonBar buttonBar = new ButtonBar();
+        addComponent(buttonBar);
+
+        Button moveUpButton = buttonBar.addButton("Move Up", FontAwesome.ARROW_UP);
+        moveUpButton.addClickListener(new MoveUpClickListener());
+
+        Button moveDownButton = buttonBar.addButton("Move Down", FontAwesome.ARROW_DOWN);
+        moveDownButton.addClickListener(new MoveDownClickListener());
+
+        table.setContainerDataSource(container);
+
+        table.setSelectable(true);
+        table.setSortEnabled(false);
+        table.setImmediate(true);
+        table.setSizeFull();
+        table.setVisibleColumns(new Object[] { "entityName", "attributeName", "width", "startPos",
+                "endPos", "transformText" });
+        table.setColumnHeaders(new String[] { "Entity Name", "Attribute Name", "Width",
+                "Start Position", "End Position", "Transform" });
+        table.setTableFieldFactory(new EditFieldFactory());
         table.setEditable(true);
         table.setDragMode(TableDragMode.ROW);
         table.setDropHandler(new TableDropHandler());
-		addComponent(table);
-		setExpandRatio(table, 1.0f);
-		
-		for (ModelEntity e : component.getInputModel().getModelEntities().values()) {
-			for (ModelAttribute a : e.getModelAttributes()) {
-				table.addItem(new AttributeFormat(e.getName(), a.getName(), 10));
-			}
-		}
-		calculatePositions();
-	}
+        addComponent(table);
+        setExpandRatio(table, 1.0f);
 
-	@Override
-	public boolean closing() {
-		return true;
-	}
+        for (ModelEntity e : component.getInputModel().getModelEntities().values()) {
+            for (ModelAttribute a : e.getModelAttributes()) {
+                table.addItem(new AttributeFormat(e.getName(), a.getName(), 10));
+            }
+        }
+        calculatePositions();
+    }
 
-	@Override
-	public void showing() {
-	}
+    @Override
+    public boolean closing() {
+        return true;
+    }
 
-	protected void calculatePositions() {
-		int pos = 1;
-		for (AttributeFormat a : container.getItemIds()) {
-			a.setStartPos(pos);
-			a.setEndPos(pos + a.getWidth());
-			pos += a.getWidth() + 1;
-		}
-	}		
+    @Override
+    public void showing() {
+    }
 
-	class MoveUpClickListener implements ClickListener {
-		public void buttonClick(ClickEvent event) {
-			Object itemId = table.getValue();
-			int index = container.indexOfId(itemId);
-			if (index != 0) {
-				container.removeItem(itemId);		
-				container.addItemAt(index - 1, itemId);
-				calculatePositions();
-			}
-		}
-	}
+    protected void calculatePositions() {
+        int pos = 1;
+        boolean needsRefreshed = false;
+        for (AttributeFormat a : container.getItemIds()) {
+            if (a.getStartPos() != pos) {
+            a.setStartPos(pos);
+            needsRefreshed = true;
+            }
+            long endPos = pos + a.getWidth();
+            if (a.getEndPos() != endPos) {
+                a.setEndPos(endPos);
+                needsRefreshed = true;
+            }
+            
+            pos += a.getWidth() + 1;
+        }
 
-	class MoveDownClickListener implements ClickListener {
-		public void buttonClick(ClickEvent event) {
-			Object itemId = table.getValue();
-			Object nextItemId = container.nextItemId(itemId);
-			if (nextItemId != null) {
-				container.removeItem(itemId);
-				container.addItemAfter(nextItemId, itemId);
-				calculatePositions();
-			}
-		}
-	}
+        if (needsRefreshed) {   
+            editables.clear();
+            table.refreshRowCache();
+        }
+    }
 
-	class EditFieldFactory implements TableFieldFactory {
-		public Field<?> createField(Container container, final Object itemId, final Object propertyId, com.vaadin.ui.Component uiContext) {
-			if (propertyId.equals("width") || propertyId.equals("transformText")) {
-				final AttributeFormat attributeFormat = (AttributeFormat) itemId;
-				TextField textField = new TextField();
-				textField.setData(itemId);
-				textField.setImmediate(true);
-				textField.addFocusListener(new FocusListener() {
-					public void focus(FocusEvent event) {
-						table.select(((TextField) event.getSource()).getData());
-					}
-				});
+    class MoveUpClickListener implements ClickListener {
+        public void buttonClick(ClickEvent event) {
+            Object itemId = table.getValue();
+            int index = container.indexOfId(itemId);
+            if (index != 0) {
+                container.removeItem(itemId);
+                container.addItemAt(index - 1, itemId);
+                calculatePositions();
+            }
+        }
+    }
 
-				textField.addValueChangeListener(new ValueChangeListener() {
-					public void valueChange(ValueChangeEvent event) {
-						
-						/*
-						String value = (String) event.getProperty().getValue();
-						if (propertyId.equals("width")) {
-							attributeFormat.setWidth(Long.parseLong((String) event.getProperty().getValue()));
-							calculatePositions();
-						} else {
-							attributeFormat.setTransformText((String) event.getProperty().getValue());
-						}
-						*/
-					}
-				});
-				textField.addBlurListener(new BlurListener() {
+    class MoveDownClickListener implements ClickListener {
+        public void buttonClick(ClickEvent event) {
+            Object itemId = table.getValue();
+            Object nextItemId = container.nextItemId(itemId);
+            if (nextItemId != null) {
+                container.removeItem(itemId);
+                container.addItemAfter(nextItemId, itemId);
+                calculatePositions();
+            }
+        }
+    }
+    
+    List<TextField> editables = new ArrayList<TextField>();
+    int focusIndex;
 
-					@Override
-					public void blur(BlurEvent event) {
-						// TODO Auto-generated method stub
-//						table.setEditable(true);
-						calculatePositions();
-						table.refreshRowCache();
-					}
-					
-				});
+    class EditFieldFactory implements TableFieldFactory {
+        public Field<?> createField(Container container, final Object itemId,
+                final Object propertyId, com.vaadin.ui.Component uiContext) {
+            if (propertyId.equals("width") || propertyId.equals("transformText")) {
+                final AttributeFormat attributeFormat = (AttributeFormat) itemId;
+                final TextField textField = new TextField();
+                textField.setData(itemId);
+                textField.setImmediate(true);
+                textField.addValueChangeListener(new ValueChangeListener() {
 
-				return textField;
-			}
-			return null;
-		}	
-	}
-	
-	class TableDropHandler implements DropHandler {
-		public void drop(DragAndDropEvent event) {
-			AbstractSelectTargetDetails targetDetails = (AbstractSelectTargetDetails) event.getTargetDetails();
-			AttributeFormat target = (AttributeFormat) targetDetails.getItemIdOver();
-			if (target != null) {
-				Transferable transferable = event.getTransferable();
-				if (transferable.getSourceComponent() == table) {
-					AttributeFormat source = (AttributeFormat) transferable.getData("itemId");
-					int diff = container.indexOfId(source) - container.indexOfId(target);
-					if (diff != 0) {
-						if (diff > 0) {
-							target = container.prevItemId(target);
-						}
-						container.removeItem(source);
-						container.addItemAfter(target, source);
-						calculatePositions();
-					}
-				}
-			}
-		}
+                    @Override
+                    public void valueChange(ValueChangeEvent event) {
+                        calculatePositions();
+                    }
+                });
+                 textField.addFocusListener(new FocusListener() {
+                 public void focus(FocusEvent event) {
+                     focusIndex = editables.indexOf(textField) + 1;
+                     table.select(((TextField) event.getSource()).getData());
+                 }
+                 });
+                editables.add(textField);
+                if (editables.size()-1 == focusIndex) {
+                    textField.focus();
+                }
+                return textField;
+            }
+            return null;
+        }
+    }
 
-		public AcceptCriterion getAcceptCriterion() {
-			return AcceptAll.get();
-		}
-	}
+    class TableDropHandler implements DropHandler {
+        public void drop(DragAndDropEvent event) {
+            AbstractSelectTargetDetails targetDetails = (AbstractSelectTargetDetails) event
+                    .getTargetDetails();
+            AttributeFormat target = (AttributeFormat) targetDetails.getItemIdOver();
+            if (target != null) {
+                Transferable transferable = event.getTransferable();
+                if (transferable.getSourceComponent() == table) {
+                    AttributeFormat source = (AttributeFormat) transferable.getData("itemId");
+                    int diff = container.indexOfId(source) - container.indexOfId(target);
+                    if (diff != 0) {
+                        if (diff > 0) {
+                            target = container.prevItemId(target);
+                        }
+                        container.removeItem(source);
+                        container.addItemAfter(target, source);
+                        calculatePositions();
+                    }
+                }
+            }
+        }
 
-	public class AttributeFormat  {
-		String entityName;
-		
-		String attributeName;
-		
-		long width;
-		
-		long startPos;
-		
-		long endPos;
-		
-		String transformText = "";
+        public AcceptCriterion getAcceptCriterion() {
+            return AcceptAll.get();
+        }
+    }
 
-		public AttributeFormat(String entityName, String attributeName, long width) {
-			this.entityName = entityName;
-			this.attributeName = attributeName;
-			this.width = width;
-		}
+    public class AttributeFormat {
+        String entityName;
 
-	    public int hashCode() {
-	        return entityName.hashCode() + attributeName.hashCode();
-	    }
-	    
-	    public boolean equals(Object obj) {
-	    	if (obj instanceof AttributeFormat) {
-	    		return hashCode() == ((AttributeFormat) obj).hashCode();
-	    	}
-	    	return super.equals(obj);
-	    }
+        String attributeName;
 
-		public String getEntityName() {
-			return entityName;
-		}
+        long width;
 
-		public void setEntityName(String entityName) {
-			this.entityName = entityName;
-		}
+        long startPos;
 
-		public String getAttributeName() {
-			return attributeName;
-		}
+        long endPos;
 
-		public void setAttributeName(String attributeName) {
-			this.attributeName = attributeName;
-		}
+        String transformText = "";
 
-		public long getWidth() {
-			return width;
-		}
+        public AttributeFormat(String entityName, String attributeName, long width) {
+            this.entityName = entityName;
+            this.attributeName = attributeName;
+            this.width = width;
+        }
 
-		public void setWidth(long width) {
-			this.width = width;
-		}
+        public int hashCode() {
+            return entityName.hashCode() + attributeName.hashCode();
+        }
 
-		public String getTransformText() {
-			return transformText;
-		}
+        public boolean equals(Object obj) {
+            if (obj instanceof AttributeFormat) {
+                return hashCode() == ((AttributeFormat) obj).hashCode();
+            }
+            return super.equals(obj);
+        }
 
-		public void setTransformText(String transformText) {
-			this.transformText = transformText;
-		}
+        public String getEntityName() {
+            return entityName;
+        }
 
-		public long getStartPos() {
-			return startPos;
-		}
+        public void setEntityName(String entityName) {
+            this.entityName = entityName;
+        }
 
-		public void setStartPos(long startPos) {
-			this.startPos = startPos;
-		}
+        public String getAttributeName() {
+            return attributeName;
+        }
 
-		public long getEndPos() {
-			return endPos;
-		}
+        public void setAttributeName(String attributeName) {
+            this.attributeName = attributeName;
+        }
 
-		public void setEndPos(long endPos) {
-			this.endPos = endPos;
-		}
-	}
+        public long getWidth() {
+            return width;
+        }
+
+        public void setWidth(long width) {
+            this.width = width;
+        }
+
+        public String getTransformText() {
+            return transformText;
+        }
+
+        public void setTransformText(String transformText) {
+            this.transformText = transformText;
+        }
+
+        public long getStartPos() {
+            return startPos;
+        }
+
+        public void setStartPos(long startPos) {
+            this.startPos = startPos;
+        }
+
+        public long getEndPos() {
+            return endPos;
+        }
+
+        public void setEndPos(long endPos) {
+            this.endPos = endPos;
+        }
+    }
 }
