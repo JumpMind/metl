785db51bda137cc0c01f39a6b69e42790b3f6f56
elong <elong@jumpmind.com>
2015-04-06 14:12:38 -0400

    implement mapping

--- symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/MappingProcessor.java
+++ symmetric-is-core/src/main/java/org/jumpmind/symmetric/is/core/runtime/component/MappingProcessor.java
@@ -1,7 +1,19 @@
 package org.jumpmind.symmetric.is.core.runtime.component;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.jumpmind.symmetric.is.core.model.ComponentAttributeSetting;
+import org.jumpmind.symmetric.is.core.model.ModelAttribute;
+import org.jumpmind.symmetric.is.core.model.ModelEntity;
+import org.jumpmind.symmetric.is.core.runtime.EntityData;
+import org.jumpmind.symmetric.is.core.runtime.IExecutionTracker;
 import org.jumpmind.symmetric.is.core.runtime.Message;
 import org.jumpmind.symmetric.is.core.runtime.flow.IMessageTarget;
+import org.jumpmind.symmetric.is.core.runtime.resource.IResourceFactory;
 
 @ComponentDefinition(category = ComponentCategory.PROCESSOR, typeName = MappingProcessor.TYPE, inputMessage=MessageType.ENTITY_MESSAGE,
 outgoingMessage=MessageType.ENTITY_MESSAGE)
@@ -11,11 +23,55 @@ public class MappingProcessor extends AbstractComponent {
 
     public final static String ATTRIBUTE_MAPS_TO = "mapping.processor.attribute.maps.to";
 
+    Map<String, String> attrToAttrMap;
+    
+    @Override
+    public void start(IExecutionTracker executionTracker, IResourceFactory resourceFactory) {
+        super.start(executionTracker, resourceFactory);
+        attrToAttrMap = new HashMap<String, String>();
+        List<ComponentAttributeSetting> attributeSettings = flowStep.getComponent().getAttributeSettings();
+        for (ComponentAttributeSetting attributeSetting : attributeSettings) {
+            if (attributeSetting.getName().equalsIgnoreCase(ATTRIBUTE_MAPS_TO)) {
+                attrToAttrMap.put(attributeSetting.getAttributeId(), attributeSetting.getValue());
+            }
+        }
+    }
+
     @Override
     public void handle(String executionId, Message inputMessage, IMessageTarget messageTarget) {
         componentStatistics.incrementInboundMessages();
+        ArrayList<EntityData> inputRows = inputMessage.getPayload();
+        if (inputRows == null) {
+            return;
+        }
+        
+        ArrayList<EntityData> outputRows = new ArrayList<EntityData>();
+        Message outputMessage = new Message(flowStep.getId());
+        
+        for (EntityData inputRow : inputRows) {
+            EntityData outputRow = new EntityData();
+            outputRows.add(outputRow);
+            for (Entry<String, Object> attrEntry : inputRow.entrySet()) {
+                String newAttrId = attrToAttrMap.get(attrEntry.getKey());
+                if (newAttrId != null) {
+                    outputRow.put(newAttrId, attrEntry.getValue());
+                } else {
+                    outputRow.put(attrEntry.getKey(), null);
+                }
+            }
+            
+            for (ModelEntity entity : flowStep.getComponent().getOutputModel().getModelEntities()) {
+                for (ModelAttribute attr: entity.getModelAttributes()) {
+                    if (!outputRow.containsKey(attr.getId())) {
+                        outputRow.put(attr.getId(), null);
+                    }
+                }
+            }
+        }
+
         componentStatistics.incrementOutboundMessages();
-        messageTarget.put(inputMessage);
+        outputMessage.setPayload(outputRows);
+        messageTarget.put(outputMessage);
     }
 
 }
--- symmetric-is-ui/src/main/java/org/jumpmind/symmetric/is/ui/init/AppUI.java
+++ symmetric-is-ui/src/main/java/org/jumpmind/symmetric/is/ui/init/AppUI.java
@@ -4,12 +4,19 @@ import org.jumpmind.symmetric.is.ui.common.DesignAgentSelect;
 import org.jumpmind.symmetric.is.ui.common.TopBar;
 import org.jumpmind.symmetric.is.ui.common.ViewManager;
 import org.jumpmind.symmetric.ui.common.AbstractSpringUI;
+import org.jumpmind.symmetric.ui.common.ResizableWindow;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
+import com.vaadin.server.DefaultErrorHandler;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @Theme("apptheme")
@@ -23,7 +30,8 @@ public class AppUI extends AbstractSpringUI {
     
     BackgroundRefresherService backgroundRefresherService;
 
-    @Override
+    @SuppressWarnings("serial")
+	@Override
     protected void init(VaadinRequest request) {
 
         super.init(request);
@@ -52,6 +60,21 @@ public class AppUI extends AbstractSpringUI {
         root.addComponents(menu, contentArea);
         root.setExpandRatio(contentArea, 1);
 
+        UI.getCurrent().setErrorHandler(new DefaultErrorHandler() {
+            public void error(com.vaadin.server.ErrorEvent event) {
+                String intro = "Exception of type <b>";
+                String message = "";
+                for (Throwable t = event.getThrowable(); t != null; t = t.getCause()) {
+                    if (t.getCause() == null) {
+                        intro += t.getClass().getName() + "</b> with the following message:<br/><br/>";
+                        message = t.getMessage();
+                    }
+                }
+                ErrorWindow window = new ErrorWindow(intro, message);
+                window.show();
+                doDefault(event);
+            } 
+        });
     }
     
     @Override
@@ -61,6 +84,30 @@ public class AppUI extends AbstractSpringUI {
         }
         super.detach();
 
+    }
+    
+    @SuppressWarnings({ "serial" })
+    class ErrorWindow extends ResizableWindow {
+    	public ErrorWindow(String intro, String message) {
+    		super("Error");
+    		setWidth(600f, Unit.PIXELS);
+    		setHeight(300f, Unit.PIXELS);
+    		content.setMargin(true);
+    		HorizontalLayout layout = new HorizontalLayout();
+    		Label icon = new Label();
+    		icon.setIcon(new ThemeResource("images/error.png"));
+    		icon.setWidth(70f, Unit.PIXELS);
+    		layout.addComponent(icon);
+    		Label labelIntro = new Label(intro, ContentMode.HTML);
+    		labelIntro.setStyleName("large");
+    		labelIntro.setWidth(530f, Unit.PIXELS);
+    		layout.addComponent(labelIntro);
+    		addComponent(layout);
+    		Label labelMessage = new Label(message, ContentMode.HTML);
+    		addComponent(labelMessage);
+    		content.setExpandRatio(labelMessage, 1.0f);
+    		addComponent(buildButtonFooter(buildCloseButton()));
+    	}
     }
 
 }
