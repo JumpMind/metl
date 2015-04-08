84da91f3524236a2e9c4fdc9b85c98c8592c96a5
Chris Henson <chenson42@gmail.com>
2015-04-08 17:09:49 -0400

    Added other database resources that are currently open in projects to the explore view

--- symmetric-is-ui/src/main/java/org/jumpmind/symmetric/is/ui/views/ExploreView.java
+++ symmetric-is-ui/src/main/java/org/jumpmind/symmetric/is/ui/views/ExploreView.java
@@ -1,10 +1,24 @@
 package org.jumpmind.symmetric.is.ui.views;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.jumpmind.db.platform.IDatabasePlatform;
+import org.jumpmind.db.platform.JdbcDatabasePlatformFactory;
+import org.jumpmind.db.sql.SqlTemplateSettings;
+import org.jumpmind.db.util.BasicDataSourcePropertyConstants;
+import org.jumpmind.symmetric.is.core.model.ProjectVersion;
+import org.jumpmind.symmetric.is.core.model.Resource;
+import org.jumpmind.symmetric.is.core.runtime.resource.DataSourceResource;
 import org.jumpmind.symmetric.is.ui.common.AppConstants;
+import org.jumpmind.symmetric.is.ui.common.ApplicationContext;
 import org.jumpmind.symmetric.is.ui.common.Category;
 import org.jumpmind.symmetric.is.ui.common.TopBarLink;
 import org.jumpmind.symmetric.ui.common.UiComponent;
@@ -21,81 +35,137 @@ import com.vaadin.ui.VerticalLayout;
 
 @UiComponent
 @Scope("ui")
-@TopBarLink(id="explore", category=Category.EXPLORE, menuOrder=20, name = "Explore", icon=FontAwesome.DATABASE)
+@TopBarLink(id = "explore", category = Category.EXPLORE, menuOrder = 20, name = "Explore", icon = FontAwesome.DATABASE)
 public class ExploreView extends VerticalLayout implements View {
 
     private static final long serialVersionUID = 1L;
 
     @Autowired
     IDatabasePlatform platform;
-    
+
+    @Autowired
+    ApplicationContext context;
+
+    List<IDb> dbs = new ArrayList<IDb>();
+
     public ExploreView() {
         setSizeFull();
+    }
+    
+    @PostConstruct
+    protected void init () {
         SqlExplorer explorer = new SqlExplorer(System.getProperty("java.io.tmpdir"),
                 new IDbProvider() {
-                    List<IDb> dbs;
-
                     @Override
                     public List<IDb> getDatabases() {
-                        if (dbs == null) {
-                            dbs = new ArrayList<IDb>();
-                            dbs.add(new IDb() {
-                                @Override
-                                public IDatabasePlatform getPlatform() {
-                                    return platform;
-                                }
-
-                                @Override
-                                public String getName() {
-                                    return "SymmetricIS DB";
-                                }
-                            });
-
-                            // final IConfigurationService configurationService
-                            // = ctx
-                            // .getBean(IConfigurationService.class);
-                            // final IConnectionFactory connectionFactory = ctx
-                            // .getBean(IConnectionFactory.class);
-                            //
-                            // List<String> types = connectionFactory
-                            // .getConnectionTypes(ConnectionCategory.DATASOURCE);
-                            // List<Connection> connections =
-                            // configurationService
-                            // .findConnectionsByTypes(types.toArray(new
-                            // String[types.size()]));
-                            // for (final Connection connection : connections) {
-                            // dbs.add(new IDb() {
-                            //
-                            // IDatabasePlatform platform;
-                            //
-                            // @Override
-                            // public IDatabasePlatform getPlatform() {
-                            // if (platform == null) {
-                            // DataSource dataSource = connectionFactory.create(
-                            // connection).reference();
-                            // platform = JdbcDatabasePlatformFactory
-                            // .createNewPlatformInstance(dataSource,
-                            // new SqlTemplateSettings(), false);
-                            // }
-                            // return platform;
-                            // }
-                            //
-                            // @Override
-                            // public String getName() {
-                            // return connection.getName();
-                            // }
-                            // });
-                            // }
+                        for (IDb db : dbs) {
+                            BasicDataSource ds = db.getPlatform().getDataSource();
+                            try {
+                                ds.close();
+                            } catch (SQLException e) {
+                            }
                         }
+
+                        dbs.clear();
+
+                        dbs.add(new IDb() {
+                            @Override
+                            public IDatabasePlatform getPlatform() {
+                                return platform;
+                            }
+
+                            @Override
+                            public String getName() {
+                                return "SymmetricIS DB";
+                            }
+                        });
+
+                        List<ProjectVersion> projects = context.getOpenProjects();
+                        for (ProjectVersion projectVersion : projects) {
+                            List<Resource> resources = context.getConfigurationService()
+                                    .findResourcesInProject(projectVersion.getId());
+                            for (Resource resource : resources) {
+                                if (resource.getType().equals(DataSourceResource.TYPE)) {
+                                    DbResource db = new DbResource(projectVersion, resource);
+                                    dbs.add(db);
+                                }
+                            }
+                        }
+
+                        Collections.sort(dbs, new Comparator<IDb>() {
+                            @Override
+                            public int compare(IDb o1, IDb o2) {
+                                return o1.getName().compareTo(o2.getName());
+                            }
+                        });
+
                         return dbs;
                     }
                 }, "admin", AppConstants.DEFAULT_LEFT_SPLIT);
         addComponent(explorer);
         explorer.refresh();
-
     }
+
     @Override
     public void enter(ViewChangeEvent event) {
+    }
+
+    class DbResource implements IDb {
+
+        Resource resource;
+
+        ProjectVersion projectVersion;
+
+        IDatabasePlatform platform;
+
+        public DbResource(ProjectVersion projectVersion, Resource resource) {
+            this.resource = resource;
+            this.projectVersion = projectVersion;
+        }
+
+        @Override
+        public String getName() {
+            return projectVersion.getName() + " " + resource.getName();
+        }
+
+        @Override
+        public IDatabasePlatform getPlatform() {
+            if (platform == null) {
+                resource.put(BasicDataSourcePropertyConstants.DB_POOL_INITIAL_SIZE, "2");
+                resource.put(BasicDataSourcePropertyConstants.DB_POOL_MAX_ACTIVE, "2");
+                resource.put(BasicDataSourcePropertyConstants.DB_POOL_MAX_IDLE, "2");
+                resource.put(BasicDataSourcePropertyConstants.DB_POOL_MIN_IDLE, "2");
+                DataSourceResource dataSourceResource = (DataSourceResource) context
+                        .getResourceFactory().create(resource);
+                DataSource dataSource = dataSourceResource.reference();
+                platform = JdbcDatabasePlatformFactory.createNewPlatformInstance(dataSource,
+                        new SqlTemplateSettings(), false);
+            }
+            return platform;
+        }
+
+        public ProjectVersion getProjectVersion() {
+            return projectVersion;
+        }
+
+        public Resource getResource() {
+            return resource;
+        }
+
+        @Override
+        public int hashCode() {
+            return resource.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof DbResource) {
+                return resource.equals(((DbResource) obj).getResource());
+            } else {
+                return super.equals(obj);
+            }
+        }
+
     }
 
 }
