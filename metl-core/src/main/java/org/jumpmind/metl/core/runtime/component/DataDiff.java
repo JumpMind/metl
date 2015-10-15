2d3a16a986c5cef76153981b01f7f56e91a5c42a
Chris Henson <chenson42@gmail.com>
2015-10-15 07:46:42 -0400

    working on data diff component

--- metl-core/src/main/java/org/jumpmind/metl/core/runtime/component/DataDiff.java
+++ metl-core/src/main/java/org/jumpmind/metl/core/runtime/component/DataDiff.java
@@ -1,16 +1,25 @@
 package org.jumpmind.metl.core.runtime.component;
 
 import java.io.File;
+import java.sql.Types;
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.h2.Driver;
+import org.jumpmind.db.model.Column;
+import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.IDatabasePlatform;
 import org.jumpmind.db.platform.JdbcDatabasePlatformFactory;
 import org.jumpmind.db.sql.SqlTemplateSettings;
 import org.jumpmind.db.util.ResettableBasicDataSource;
+import org.jumpmind.metl.core.model.DataType;
+import org.jumpmind.metl.core.model.Model;
+import org.jumpmind.metl.core.model.ModelAttribute;
+import org.jumpmind.metl.core.model.ModelEntity;
 import org.jumpmind.metl.core.runtime.LogLevel;
 import org.jumpmind.metl.core.runtime.Message;
+import org.jumpmind.metl.core.runtime.MisconfiguredException;
 import org.jumpmind.metl.core.runtime.flow.ISendMessageCallback;
 import org.jumpmind.properties.TypedProperties;
 
@@ -26,19 +35,43 @@ public class DataDiff extends AbstractComponentRuntime {
 
     String sourceStep2Id;
 
-    boolean inMemoryCompare = true;    
+    boolean inMemoryCompare = true;
 
-    IDatabasePlatform database;
+    IDatabasePlatform databasePlatform;
 
     File databaseFile;
+
+    RdbmsWriter databaseWriter;
 
     @Override
     public void handle(Message inputMessage, ISendMessageCallback messageTarget, boolean unitOfWorkBoundaryReached) {
         createDatabase();
+        String originatingStepId = inputMessage.getHeader().getOriginatingStepId();
+        String tableSuffix = null;
+        if (sourceStep1Id.equals(originatingStepId)) {
+            tableSuffix = "_1";
+        } else if (sourceStep2Id.equals(originatingStepId)) {
+            tableSuffix = "_2";
+        }
+
+        if (databaseWriter == null) {
+            databaseWriter.setDatabasePlatform(databasePlatform);
+        }
+
+        if (tableSuffix != null) {
+            databaseWriter.setTableSuffix(tableSuffix);
+            databaseWriter.handle(inputMessage, null, false);
+        }
+        
+        if (unitOfWorkBoundaryReached) {
+            // select where in _1 and not in _2 for DEL
+            // select where in _2 and not in _1 for ADD
+        }
+
     }
 
     protected void createDatabase() {
-        if (database == null) {
+        if (databasePlatform == null) {
             ResettableBasicDataSource ds = new ResettableBasicDataSource();
             ds.setDriverClassName(Driver.class.getName());
             String uuid = UUID.randomUUID().toString();
@@ -48,15 +81,45 @@ public class DataDiff extends AbstractComponentRuntime {
                 databaseFile = new File(System.getProperty("h2.baseDir"), uuid + ".h2.db");
                 ds.setUrl("jdbc:h2:file:./" + uuid);
             }
-            database = JdbcDatabasePlatformFactory.createNewPlatformInstance(ds, new SqlTemplateSettings(), false);
+            databasePlatform = JdbcDatabasePlatformFactory.createNewPlatformInstance(ds, new SqlTemplateSettings(), false);
 
-            log(LogLevel.INFO, "Creating databse with the following url: %s", ds.getUrl());
+            Model inputModel = context.getFlowStep().getComponent().getInputModel();
+            List<ModelEntity> entities = inputModel.getModelEntities();
+            for (ModelEntity entity : entities) {
+                Table table = new Table();
+                table.setName(entity.getName() + "_1");
+                List<ModelAttribute> attributes = entity.getModelAttributes();
+                for (ModelAttribute attribute : attributes) {
+                    DataType dataType = attribute.getDataType();
+                    Column column = new Column(attribute.getName());
+                    if (dataType.isNumeric()) {
+                        column.setTypeCode(Types.DECIMAL);
+                    } else if (dataType.isBoolean()) {
+                        column.setTypeCode(Types.BOOLEAN);
+                    } else if (dataType.isTimestamp()) {
+                        column.setTypeCode(Types.TIMESTAMP);
+                    } else if (dataType.isBinary()) {
+                        column.setTypeCode(Types.BLOB);
+                    } else {
+                        column.setTypeCode(Types.LONGVARCHAR);
+                    }
+                    table.addColumn(column);
+                }
+                databasePlatform.createTables(false, false, table);
+
+                table.setName(entity.getName() + "_2");
+                databasePlatform.createTables(false, false, table);
+
+            }
+
+            log(LogLevel.INFO, "Creating databasePlatform with the following url: %s", ds.getUrl());
         }
     }
-    
+
     @Override
     public void stop() {
         if (databaseFile != null) {
+            log(LogLevel.INFO, "Deleting databasePlatform file: %s", databaseFile);
             FileUtils.deleteQuietly(databaseFile);
         }
     }
@@ -68,6 +131,11 @@ public class DataDiff extends AbstractComponentRuntime {
         this.sourceStep2Id = properties.get(SOURCE_2);
         this.inMemoryCompare = properties.is(IN_MEMORY_COMPARE);
         this.rowsPerMessage = properties.getInt(ROWS_PER_MESSAGE);
+
+        Model inputModel = context.getFlowStep().getComponent().getInputModel();
+        if (inputModel == null) {
+            throw new MisconfiguredException("The input model is not set and it is required");
+        }
     }
 
     @Override
