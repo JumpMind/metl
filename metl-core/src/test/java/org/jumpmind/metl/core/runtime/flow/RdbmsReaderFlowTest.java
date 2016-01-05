31462f406612266aeb99d113e42ae9fb1f737536
gwilmer <gwilmer@jumpmind.com>
2016-01-04 21:27:21 -0500

    Dev Checkin - Wiki Doc

---
+++ metl-core/src/test/java/org/jumpmind/metl/core/runtime/flow/RdbmsReaderFlowTest.java
@@ -0,0 +1,33 @@
+package org.jumpmind.metl.core.runtime.flow;
+
+import java.util.Collection;
+
+import org.jumpmind.metl.core.model.FlowName;
+import org.jumpmind.metl.core.runtime.StandaloneTestFlowRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RdbmsReaderFlowTest {
+
+    static StandaloneTestFlowRunner standaloneFlowRunner;
+
+    FlowName flow;
+
+    public RdbmsReaderFlowTest(FlowName flow) {
+        this.flow = flow;
+    }
+
+    @Test
+    public void testFlow() throws Exception {
+        standaloneFlowRunner.testFlow(flow);
+    }
+
+    @Parameters(name = "{0}")
+    public static Collection<Object[]> getFlows() throws Exception {
+        standaloneFlowRunner = new StandaloneTestFlowRunner("/rdbmsreader-flow-test-config.sql");
+        return standaloneFlowRunner.getFlowAsTestParams();
+    }
+}
