93b855e42df6765e815c1d7980f845f21081743d
gwilmer <gwilmer@jumpmind.com>
2016-02-02 07:59:11 -0500

    Fixed #219.  SQL Executor cleanup and consistency.

--- metl-core/src/main/java/org/jumpmind/metl/core/runtime/component/SqlExecutor.java
+++ metl-core/src/main/java/org/jumpmind/metl/core/runtime/component/SqlExecutor.java
@@ -25,21 +25,21 @@ import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.jumpmind.db.sql.SqlScriptReader;
+import org.jumpmind.metl.core.runtime.ContentMessage;
 import org.jumpmind.metl.core.runtime.ControlMessage;
-import org.jumpmind.metl.core.runtime.EntityData;
-import org.jumpmind.metl.core.runtime.EntityDataMessage;
 import org.jumpmind.metl.core.runtime.LogLevel;
 import org.jumpmind.metl.core.runtime.Message;
 import org.jumpmind.metl.core.runtime.MisconfiguredException;
 import org.jumpmind.metl.core.runtime.flow.ISendMessageCallback;
 import org.jumpmind.properties.TypedProperties;
-import org.jumpmind.util.FormatUtils;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class SqlExecutor extends AbstractRdbmsComponentRuntime {
@@ -58,13 +58,42 @@ public class SqlExecutor extends AbstractRdbmsComponentRuntime {
     protected void start() {
         TypedProperties properties = getTypedProperties();
         file = properties.get(FILE);
-        sqls = getSqlStatements(isBlank(file));
+        sqls = getExecutorSqlStatements();
         runWhen = properties.get(RUN_WHEN, PER_MESSAGE);
         if (getResourceRuntime() == null) {
             throw new IllegalStateException("This component requires a data source");
         }
     }
 
+    protected List<String> getExecutorSqlStatements() {
+
+        List<String> sqlStatements=null;;
+
+        //sqlstatements come from file or sql setting in the component
+        if (isNotBlank(file)) {
+            sqlStatements = new ArrayList<String>();
+            FileReader fileReader = null;
+            SqlScriptReader sqlReader = null;
+            try {
+                fileReader = new FileReader(file);
+                sqlReader = new SqlScriptReader(fileReader);
+                String sqlToExecute = sqlReader.readSqlStatement();
+                while (sqlToExecute != null) {
+                    sqlStatements.add(sqlToExecute);
+                    sqlToExecute = sqlReader.readSqlStatement();
+                }
+            } catch (FileNotFoundException e) {
+                throw new MisconfiguredException("Could not find configured file: %s", file);
+            } finally {
+                IOUtils.closeQuietly(fileReader);
+                IOUtils.closeQuietly(sqlReader);
+            }
+        } else {
+            sqlStatements = getSqlStatements(isBlank(file));            
+        }
+        return sqlStatements;
+    }
+    
     @Override
     public boolean supportsStartupMessages() {
         return true;
@@ -74,69 +103,37 @@ public class SqlExecutor extends AbstractRdbmsComponentRuntime {
     public void handle(final Message inputMessage, final ISendMessageCallback callback, boolean unitOfWorkBoundaryReached) {
         results.clear();
         NamedParameterJdbcTemplate template = getJdbcTemplate();
-        Map<String, Object> params = getParameters(inputMessage);
         int sqlCount = 0;
-        if (isNotBlank(file)) {
-            FileReader fileReader = null;
-            SqlScriptReader sqlReader = null;
-            try {
-                fileReader = new FileReader(file);
-                sqlReader = new SqlScriptReader(fileReader);
-                String sqlToExecute = sqlReader.readSqlStatement();
-                while (sqlToExecute != null) {
-                    sqlCount += processSql(inputMessage, template, params, sqlToExecute);
-                    sqlToExecute = sqlReader.readSqlStatement();
-                }
-            } catch (FileNotFoundException e) {
-                throw new MisconfiguredException("Could not find configured file: %s", file);
-            } finally {
-                IOUtils.closeQuietly(fileReader);
-                IOUtils.closeQuietly(sqlReader);
-            }
-
-        }
-
-        for (String sql : this.sqls) {
-            final String sqlToExecute = FormatUtils.replaceTokens(sql, context.getFlowParameters(), true);
-            sqlCount += processSql(inputMessage, template, params, sqlToExecute);
-        }
-
+        int resultCount = 0;
+        int inboundRecordCount = 0;
+        
+        Iterator<?> inboundPayload = null;
+        if (PER_ENTITY.equals(runWhen) && inputMessage instanceof ContentMessage<?>) {
+            inboundPayload = ((Collection<?>) ((ContentMessage<?>) inputMessage).getPayload()).iterator();
+            inboundRecordCount = ((Collection<?>) ((ContentMessage<?>) inputMessage).getPayload()).size();
+        } else if (PER_MESSAGE.equals(runWhen) && !(inputMessage instanceof ControlMessage)) {
+            inboundPayload = null;
+            inboundRecordCount = 1;
+        } else if (PER_UNIT_OF_WORK.equals(runWhen) && inputMessage instanceof ControlMessage) {
+            inboundPayload = null;
+            inboundRecordCount = 1;
+        }        
+        
+        for (int i = 0; i < inboundRecordCount; i++) {
+            Object entity = inboundPayload != null && inboundPayload.hasNext() ? inboundPayload.next() : null;
+            for (String sql : this.sqls) {
+                String sqlToExecute = prepareSql(sql, inputMessage, entity);
+                Map<String, Object> paramMap = prepareParams(sqlToExecute, inputMessage, entity, runWhen);
+                log(LogLevel.INFO, "About to run: %s", sqlToExecute);
+                log(LogLevel.INFO, "Passing params: %s", paramMap);
+                resultCount = template.update(sqlToExecute, paramMap);  
+                getComponentStatistics().incrementNumberEntitiesProcessed(resultCount);
+                sqlCount++;
+            }            
+        }        
         if (callback != null && sqlCount > 0) {
             callback.sendTextMessage(null, convertResultsToTextPayload(results));
         }
-        
-        log(LogLevel.INFO, "Ran %d sql statements", sqlCount);
-    }
-
-    private int processSql(Message inputMessage, NamedParameterJdbcTemplate template, Map<String, Object> params, String sqlToExecute) {
-        int sqlCount = 0;
-        if (runWhen.equals(PER_UNIT_OF_WORK) && inputMessage instanceof ControlMessage) {
-            int count = template.update(sqlToExecute, params);
-            results.add(new Result(sqlToExecute, count));
-            getComponentStatistics().incrementNumberEntitiesProcessed(count);
-            sqlCount++;            
-        } else if (runWhen.equals(PER_MESSAGE) && !(inputMessage instanceof ControlMessage)) {
-            int count = template.update(sqlToExecute, params);
-            results.add(new Result(sqlToExecute, count));
-            getComponentStatistics().incrementNumberEntitiesProcessed(count);
-            sqlCount++;
-        } else if (runWhen.equals(PER_ENTITY) && inputMessage instanceof EntityDataMessage) {
-            List<EntityData> datas = ((EntityDataMessage)inputMessage).getPayload();
-            for (EntityData entityData : datas) {
-                params.putAll(getComponent().toRow(entityData, false, true));
-                int count = template.update(sqlToExecute, params);
-                results.add(new Result(sqlToExecute, count));
-                getComponentStatistics().incrementNumberEntitiesProcessed(count);
-                sqlCount++;
-            }
-        }
-        return sqlCount;
-    }
-
-    private HashMap<String, Object> getParameters(Message inputMessage) {
-        HashMap<String, Object> params = new HashMap<String, Object>(context.getFlowParameters());
-        params.putAll(inputMessage.getHeader());
-        return params;
-    }
-
+        log(LogLevel.INFO, "Ran %d sql statements", sqlCount);        
+    } 
 }
