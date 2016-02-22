52f2b53fe55832950816b7709a2d23e2f1e6665c
Chris Henson <chenson42@gmail.com>
2016-02-22 16:31:59 -0500

    Binary File Writer doesn't close outputstream Fixed #245

--- metl-core/src/main/java/org/jumpmind/metl/core/runtime/component/BinaryFileWriter.java
+++ metl-core/src/main/java/org/jumpmind/metl/core/runtime/component/BinaryFileWriter.java
@@ -15,44 +15,43 @@ import org.jumpmind.metl.core.runtime.resource.IDirectory;
 public class BinaryFileWriter extends AbstractFileWriter {
 
     public static final String TYPE = "Binary File Writer";
-    OutputStream fos = null;
-    
-	@Override
-	protected void start() {
-		init();
-	}
-	
-	@Override
-	public void handle(Message inputMessage, ISendMessageCallback callback, boolean unitOfWorkBoundaryReached) {
-		
-		IDirectory streamable;
-		if (inputMessage instanceof BinaryMessage) {
-		    BinaryMessage message = (BinaryMessage)inputMessage;
-			String fileName = getFileName(inputMessage);
-			streamable = initStream(fileName);
-			initFos(streamable, fileName);
-			try {
-				fos.write(message.getPayload());
-			} catch(IOException e) {
-				throw new IoException(e);
-			}
-		} 
-		
-        if ((inputMessage instanceof ControlMessage ||
-        		unitOfWorkBoundaryReached) && callback != null) {
-        	IOUtils.closeQuietly(fos);
-        	ArrayList<String> results = new ArrayList<>();
-        	results.add("{\"status\":\"success\"}");
-            callback.sendTextMessage(null, results);			
-		}
-	}
 
-	@Override
-	public boolean supportsStartupMessages() {
-		return false;
-	}
-	
-	private void initFos(IDirectory streamable, String fileName) {
-		fos = streamable.getOutputStream(fileName, mustExist);
-	}
+    @Override
+    protected void start() {
+        init();
+    }
+
+    @Override
+    public void handle(Message inputMessage, ISendMessageCallback callback, boolean unitOfWorkBoundaryReached) {
+
+        IDirectory streamable = (IDirectory) getResourceReference();
+        if (inputMessage instanceof BinaryMessage) {
+            BinaryMessage message = (BinaryMessage) inputMessage;
+            String fileName = getFileName(inputMessage);
+            OutputStream fos = streamable.getOutputStream(fileName, mustExist);
+            if (!append) {
+                streamable.delete(fileName);
+            }
+
+            try {
+                fos.write(message.getPayload());
+            } catch (IOException e) {
+                throw new IoException(e);
+            } finally {
+                IOUtils.closeQuietly(fos);
+            }
+        }
+
+        if ((inputMessage instanceof ControlMessage || unitOfWorkBoundaryReached) && callback != null) {
+            ArrayList<String> results = new ArrayList<>();
+            results.add("{\"status\":\"success\"}");
+            callback.sendTextMessage(null, results);
+        }
+    }
+
+    @Override
+    public boolean supportsStartupMessages() {
+        return false;
+    }
+
 }
