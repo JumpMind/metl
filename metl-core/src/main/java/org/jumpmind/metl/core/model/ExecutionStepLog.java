462eb25f6748de5b7e9fd359f9bf65ebbb735f1c
Chris Henson <chenson42@gmail.com>
2016-04-04 10:44:10 -0400

    Get rid of abbreviation of log messages

--- metl-core/src/main/java/org/jumpmind/metl/core/model/ExecutionStepLog.java
+++ metl-core/src/main/java/org/jumpmind/metl/core/model/ExecutionStepLog.java
@@ -71,7 +71,7 @@ public class ExecutionStepLog extends AbstractObject implements Comparable<Execu
 	}
 
 	public void setLogText(String logText) {
-		this.logText = StringUtils.abbreviate(logText, 3900);
+		this.logText = logText;
 	}
 
 	public Date getCreateTime() {
