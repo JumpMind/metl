c944451b6f6834643bae0e24f0ad438229468b06
Chris Henson <chenson42@gmail.com>
2016-06-09 13:07:01 -0400

    sample custom component

---
+++ metl-sample-component/src/main/java/org/jumpmind/metl/sample/Sample.java
@@ -0,0 +1,25 @@
+package org.jumpmind.metl.sample;
+
+import org.jumpmind.metl.core.runtime.Message;
+import org.jumpmind.metl.core.runtime.component.AbstractComponentRuntime;
+import org.jumpmind.metl.core.runtime.flow.ISendMessageCallback;
+
+public class Sample extends AbstractComponentRuntime {
+
+    public Sample() {
+    }
+
+    @Override
+    public void handle(Message inputMessage, ISendMessageCallback callback, boolean unitOfWorkBoundaryReached) {
+    }
+
+    @Override
+    public boolean supportsStartupMessages() {
+        return false;
+    }
+
+    @Override
+    protected void start() {
+    }
+
+}
---
+++ metl-sample-component/src/main/java/org/jumpmind/metl/sample/SampleUIPanel.java
@@ -0,0 +1,16 @@
+package org.jumpmind.metl.sample;
+
+import org.jumpmind.metl.ui.views.design.AbstractComponentEditPanel;
+
+public class SampleUIPanel extends AbstractComponentEditPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    public SampleUIPanel() {
+    }
+
+    @Override
+    protected void buildUI() {
+    }
+
+}
