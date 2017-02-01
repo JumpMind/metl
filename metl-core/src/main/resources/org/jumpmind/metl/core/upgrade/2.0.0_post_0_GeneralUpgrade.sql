b070144e82da0a8395fd2ab3c2a3d7f76e284c76
gwilmer <gwilmer@jumpmind.com>
2017-02-01 08:40:11 -0500

    Dev checkin #324

--- metl-core/src/main/resources/org/jumpmind/metl/core/upgrade/2.0.0_post_0_GeneralUpgrade.sql
+++ metl-core/src/main/resources/org/jumpmind/metl/core/upgrade/2.0.0_post_0_GeneralUpgrade.sql
@@ -11,11 +11,15 @@ where id in
             project_id, max(create_time) max_create_time
          from 
             metl_project_version
+         where
+            deleted=0
          group by
             project_id
       ) mct
          on mpv.project_id = mct.project_id
          and mpv.create_time = mct.max_create_time
+   where 
+      mpv.deleted=0
 );
 
 update metl_project_version set version_type='RELEASE', release_date='2017-01-01'
