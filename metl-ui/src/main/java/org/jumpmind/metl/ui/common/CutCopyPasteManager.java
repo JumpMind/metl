5723f8e1124a4954ac07acc5bfb97d5297b24023
gwilmer <gwilmer@jumpmind.com>
2017-03-07 18:29:34 -0500

    Copy Paste Model Fix.

--- metl-ui/src/main/java/org/jumpmind/metl/ui/common/CutCopyPasteManager.java
+++ metl-ui/src/main/java/org/jumpmind/metl/ui/common/CutCopyPasteManager.java
@@ -118,7 +118,7 @@ public class CutCopyPasteManager {
             // make another copy if same project version. If diff project
             // version try and use existing
             if (existingResourceId == null
-                    || resource.getProjectVersionId() == newProjectVersionId) {
+                    || resource.getProjectVersionId().equalsIgnoreCase(newProjectVersionId)) {
                 // make a copy only if the resource is still in use by another
                 // flow
                 // resource alone can't be cut if they have dependent flows
@@ -276,7 +276,7 @@ public class CutCopyPasteManager {
         HashSet<Model> newModels = new HashSet<Model>();
         for (Model model : origModels) {
             String existingModelId = destinationHasModel(model, newProjectVersionId);
-            if (existingModelId == null || model.getProjectVersionId() == newProjectVersionId) {
+            if (existingModelId == null || model.getProjectVersionId().equalsIgnoreCase(newProjectVersionId)) {
                 // make a copy only if the model is still in use by another flow
                 // model alone can't be cut if they have dependent flows
                 if ((clipboard.containsKey(CLIPBOARD_ACTION)
