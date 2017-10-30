0bb9c721436ed97689111f222f2d93aebb14f511
nrichardson <nrichardson@10.162.41.214>
2017-10-30 08:25:26 -0400

    Closed #674 include attachments on email writer

--- comp-email/src/main/java/org/jumpmind/metl/core/runtime/component/EmailWriter.java
+++ comp-email/src/main/java/org/jumpmind/metl/core/runtime/component/EmailWriter.java
@@ -2,27 +2,44 @@ package org.jumpmind.metl.core.runtime.component;
 
 import static org.apache.commons.lang.StringUtils.isBlank;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipOutputStream;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 
+import org.jumpmind.exception.IoException;
 import org.jumpmind.metl.core.model.Model;
 import org.jumpmind.metl.core.runtime.ControlMessage;
 import org.jumpmind.metl.core.runtime.EntityData;
 import org.jumpmind.metl.core.runtime.EntityDataMessage;
+import org.jumpmind.metl.core.runtime.LogLevel;
 import org.jumpmind.metl.core.runtime.Message;
 import org.jumpmind.metl.core.runtime.MisconfiguredException;
 import org.jumpmind.metl.core.runtime.TextMessage;
 import org.jumpmind.metl.core.runtime.flow.ISendMessageCallback;
+import org.jumpmind.metl.core.runtime.resource.FileInfo;
+import org.jumpmind.metl.core.runtime.resource.IDirectory;
+import org.jumpmind.metl.core.runtime.resource.IResourceRuntime;
 import org.jumpmind.metl.core.runtime.resource.MailSession;
 import org.jumpmind.util.FormatUtils;
 
@@ -39,6 +56,8 @@ public class EmailWriter extends AbstractComponentRuntime {
     final static String SOURCE_STEP_EMAIL_ADDRESS = "source.step.email.addresses";
     final static String SOURCE_STEP_EMAIL_ADDRESS_TYPE = "source.step.email.addresses.type";
     final static String ONE_EMAIL_PER_RECIPIENT = "one.email.per.recipient";
+    final static String INCLUDE_ATTACHMENT = "include.attachment";
+    final static String ATTACHMENT_FILE_PATH = "attachment.file.path";
 
     final static String VALUE_SOURCE_STEP_EMAIL_ADDRESS_TYPE_TO = "TO";
     final static String VALUE_SOURCE_STEP_EMAIL_ADDRESS_TYPE_CC = "CC";
@@ -49,9 +68,11 @@ public class EmailWriter extends AbstractComponentRuntime {
     List<Message> queuedMessages = new ArrayList<>();
 
     List<String> recipients = new ArrayList<>();
+    
+    List<String> attachmentFiles = new ArrayList<>();
 
     MailSession mailSession;
-    
+
     String subType;
 
     String bodyCharSet;
@@ -60,6 +81,7 @@ public class EmailWriter extends AbstractComponentRuntime {
     public void start() {
         super.start();
         recipientsReady = false;
+
         mailSession = getResourceRuntime() != null ? getResourceRuntime().reference() : null;
         if (mailSession == null) {
             info("Using global mail session because a resource was not configured");
@@ -145,12 +167,15 @@ public class EmailWriter extends AbstractComponentRuntime {
             } else if (VALUE_SOURCE_STEP_EMAIL_ADDRESS_TYPE_BCC.equals(recipientType)) {
                 recipientBuilder = bcc;
             }
+
+            StringBuilder attachmentFilePath = new StringBuilder(properties.get(ATTACHMENT_FILE_PATH,""));
+            
             boolean oneMessagePerRecipient = properties.is(ONE_EMAIL_PER_RECIPIENT);
             for (String recipient : recipients) {
                 recipientBuilder.append(",").append(recipient);
                 if (oneMessagePerRecipient) {
                     sendEmail(inputMessage, to.toString(), cc.toString(), bcc.toString(),
-                            from.toString(), subject, body, callback);
+                            from.toString(), subject, body, callback, attachmentFilePath.toString());
 
                     recipientBuilder.replace(recipientBuilder.length() - recipient.length() - 1,
                             recipientBuilder.length(), "");
@@ -159,13 +184,13 @@ public class EmailWriter extends AbstractComponentRuntime {
 
             if (!oneMessagePerRecipient) {
                 sendEmail(inputMessage, to.toString(), cc.toString(), bcc.toString(),
-                        from.toString(), subject, body, callback);
+                        from.toString(), subject, body, callback, attachmentFilePath.toString());
             }
         }
     }
 
     protected void sendEmail(Message inputMessage, String to, String cc, String bcc, String from,
-            String subject, String body, ISendMessageCallback callback) throws MessagingException {
+            String subject, String body, ISendMessageCallback callback, String attachmentFilePath) throws MessagingException {
         String runWhen = properties.get(RUN_WHEN);
         to = resolveParamsAndHeaders(to, inputMessage);
         cc = resolveParamsAndHeaders(cc, inputMessage);
@@ -173,13 +198,14 @@ public class EmailWriter extends AbstractComponentRuntime {
         from = resolveParamsAndHeaders(from, inputMessage);
         subject = resolveParamsAndHeaders(subject, inputMessage);
         body = resolveParamsAndHeaders(body, inputMessage);
+        attachmentFilePath = resolveParamsAndHeaders(attachmentFilePath, inputMessage);
         if (PER_ENTITY.equals(runWhen)) {
             if (inputMessage instanceof TextMessage) {
                 List<String> payload = ((TextMessage) inputMessage).getPayload();
                 for (String text : payload) {
                     sendEmail(to.toString(), cc.toString(), bcc.toString(), from.toString(),
                             FormatUtils.replaceToken(subject, "text", text, true),
-                            FormatUtils.replaceToken(body, "text", text, true), callback);
+                            FormatUtils.replaceToken(body, "text", text, true), callback, attachmentFilePath.toString());
                 }
             } else if (inputMessage instanceof EntityDataMessage) {
                 List<EntityData> payload = ((EntityDataMessage) inputMessage).getPayload();
@@ -193,16 +219,16 @@ public class EmailWriter extends AbstractComponentRuntime {
                                 "The input model is required if " + PER_ENTITY + " is selected");
                     }
                     sendEmail(to, cc, bcc, from, FormatUtils.replaceTokens(subject, row, true),
-                            FormatUtils.replaceTokens(body, row, true), callback);
+                            FormatUtils.replaceTokens(body, row, true), callback, attachmentFilePath);
                 }
             }
         } else {
-            sendEmail(to, cc, bcc, from, subject, body, callback);
+            sendEmail(to, cc, bcc, from, subject, body, callback, attachmentFilePath);
         }
     }
 
     protected void sendEmail(String to, String cc, String bcc, String from, String subject,
-            String body, ISendMessageCallback callback) throws MessagingException {
+            String body, ISendMessageCallback callback, String attachmentFilePath) throws MessagingException {
         Transport transport = null;
         try {
         	bodyCharSet = properties.get(BODY_CHARSET, "utf-8");
@@ -221,8 +247,30 @@ public class EmailWriter extends AbstractComponentRuntime {
             mailMessage.setRecipients(RecipientType.TO, to);
             mailMessage.setFrom(new InternetAddress(from));
             mailMessage.setSubject(subject);
-            mailMessage.setText(body, bodyCharSet, subType);
-            transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
+
+            // If including an attachment
+            BodyPart msgBodyPart = new MimeBodyPart();
+            Multipart multipart = new MimeMultipart();
+            if(properties.is(INCLUDE_ATTACHMENT)) {
+            	attachmentFiles = Arrays.asList(attachmentFilePath.split("\\|"));
+            	msgBodyPart.setText(body);
+                multipart.addBodyPart(msgBodyPart);
+                for (String filename : attachmentFiles) {
+                	info("Attaching file: %s", filename.trim());
+                    msgBodyPart = new MimeBodyPart();
+                    DataSource source = new FileDataSource(filename.trim());
+                    msgBodyPart.setDataHandler(new DataHandler(source));
+
+                    msgBodyPart.setFileName(filename.trim());
+                    multipart.addBodyPart(msgBodyPart);
+        		}
+                mailMessage.setContent(multipart);
+            }
+            else {
+            	mailMessage.setText(body, bodyCharSet, subType);
+            }
+
+          	transport.sendMessage(mailMessage, mailMessage.getAllRecipients());
             
             Map<String,Serializable> header = new LinkedHashMap<>();
             header.put("to", to);
