e6b73cfde5e6fee792331ba6594ab65ede4b93ca
gregwilmer <gregwilmer@192.168.42.101>
2018-10-30 13:45:33 -0400

    Dev #870 - Work on upgrade scripts.

--- metl-core/src/main/resources/org/jumpmind/metl/core/upgrade/3.3.0_post_0_GeneralUpgrade.sql
+++ metl-core/src/main/resources/org/jumpmind/metl/core/upgrade/3.3.0_post_0_GeneralUpgrade.sql
@@ -1,6 +1,3 @@
-insert into metl_hierarchical_model select * from metl_model where upper(type)='HIERARCHICAL'
-
-insert into metl_relational_model select * from metl_model where upper(type)='RELATIONAL'
 
 insert into metl_component_model_setting (id, type, component_id, model_object_id, name, value, create_time, create_by)
 select id, 'ATTRIBUTE', component_id, attribute_id, 'mapping.processor.model.object.maps.to', value, create_time, create_by 
