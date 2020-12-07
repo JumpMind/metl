b78afe0a1389e044916c030d49ca8c5d9e0daa6a
Bryan Lutz <bryan.lutz@leadingedje.com>
2020-12-07 16:39:53 -0500

    SQS Reader now has a delete after read option

--- comp-sqs/src/main/java/org/jumpmind/metl/core/runtime/component/SQSReader.java
+++ comp-sqs/src/main/java/org/jumpmind/metl/core/runtime/component/SQSReader.java
@@ -29,40 +29,44 @@ import org.jumpmind.metl.core.runtime.flow.ISendMessageCallback;
 import org.jumpmind.properties.TypedProperties;
 
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
 public class SQSReader extends AbstractComponentRuntime {
 
     public final static String TYPE = "SQS Reader";
 
     public final static String SQS_READER_QUEUE_URL = "sqs.reader.queue.url";
-    public final static String SQS_READER_MAX_MESSAGES_READ_AT_ONCE = "sqs.reader.max.messages.read.at.once";     
-    public final static String SQS_READER_QUEUE_MESSAGES_PER_OUTPUT_MESSAGE = "sqs.reader.queue.messages.per.output.message";     
-    public final static String SQS_READER_READ_UNTIL_QUEUE_EMPTY = "sqs.reader.read.until.queue.empty";
+    public final static String SQS_READER_MAX_MESSAGES_READ_AT_ONCE = "sqs.reader.max.messages.read.at.once";
     public final static String SQS_READER_DELETE_AFTER_READ = "sqs.reader.delete.after.read";
-    public final static String SQS_READER_RUN_WHEN = "sqs.reader.run.when";    
+    //public final static String SQS_READER_QUEUE_MESSAGES_PER_OUTPUT_MESSAGE = "sqs.reader.queue.messages.per.output.message";
+    //public final static String SQS_READER_READ_UNTIL_QUEUE_EMPTY = "sqs.reader.read.until.queue.empty";
     
     /* settings */
-    String queueUrl;
     String runWhen;
+    String queueUrl;
     Integer maxMsgsToReadAtOnce;
-    boolean readUntilQueueEmpty;
+    boolean deleteAfterRead;
+    //boolean readUntilQueueEmpty;
 
     @Override
     public void start() {
         if (getResourceRuntime() == null) {
             throw new IllegalStateException("SQS queue resource must be defined");
         }
-        
+
         TypedProperties properties = getTypedProperties();
+        runWhen = properties.get(RUN_WHEN);
         queueUrl = properties.get(SQS_READER_QUEUE_URL);
         maxMsgsToReadAtOnce = properties.getInt(SQS_READER_MAX_MESSAGES_READ_AT_ONCE);
-        readUntilQueueEmpty = Boolean.valueOf(properties.getProperty(SQS_READER_READ_UNTIL_QUEUE_EMPTY));
-        
+        deleteAfterRead = Boolean.valueOf(properties.getProperty(SQS_READER_DELETE_AFTER_READ));
+
+        //readUntilQueueEmpty = Boolean.valueOf(properties.getProperty(SQS_READER_READ_UNTIL_QUEUE_EMPTY));
+
         if (maxMsgsToReadAtOnce < 1 || maxMsgsToReadAtOnce  > 10) {
-            throw new MisconfiguredException("This must be blah");
+            throw new MisconfiguredException("\"Max Messages to Read at Once\" must be between 1 and 10");
         }
-        //readUntilQueueEmpty = properties.getInt(SQS_READER_READ_UNTIL_QUEUE_EMPTY);
     }
 
     @Override
@@ -70,24 +74,40 @@ public class SQSReader extends AbstractComponentRuntime {
         return true;
     }
 
-	@Override
+    @Override
     public void handle(Message inputMessage, ISendMessageCallback callback, boolean unitOfWorkBoundaryReached) {
-		if (PER_MESSAGE.equals(runWhen) && (!(inputMessage instanceof ControlMessage) || context.isStartStep())
-				|| (PER_UNIT_OF_WORK.equals(runWhen) && inputMessage instanceof ControlMessage)) {
-			
-			SqsClient client = (SqsClient)getResourceReference();
-			
+        if (PER_MESSAGE.equals(runWhen) && (!(inputMessage instanceof ControlMessage) || context.isStartStep())
+                || (PER_UNIT_OF_WORK.equals(runWhen) && inputMessage instanceof ControlMessage)) {
+
+            SqsClient client = (SqsClient)getResourceReference();
+
             ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
                     .queueUrl(queueUrl)
                     .maxNumberOfMessages(maxMsgsToReadAtOnce)
                     .build();
 
             ArrayList<String> outputMessages = new ArrayList<String>();
-            outputMessages.add("***********************");
-            outputMessages.add("AWS SQS READER");
-            outputMessages.add("***********************");
-            callback.sendTextMessage(null, outputMessages);
+            ReceiveMessageResponse response = client.receiveMessage(receiveRequest);
 
+            response.messages().forEach(message -> {
+                outputMessages.add(message.body());
+
+                deleteMessage(client, message);
+            });
+
+            callback.sendTextMessage(null, outputMessages);
+        }
+
+    }
+
+    private void deleteMessage(SqsClient client, software.amazon.awssdk.services.sqs.model.Message message) {
+        if (deleteAfterRead) {
+            DeleteMessageRequest request1 = DeleteMessageRequest.builder()
+                    .queueUrl(queueUrl)
+                    .receiptHandle(message.receiptHandle())
+                    .build();
+
+            client.deleteMessage(request1);
         }
     }
 }
