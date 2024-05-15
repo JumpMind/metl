7be4b7e35f88de1a2f70a1e350146ad0ab56fdfa
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2024-05-15 08:40:23 -0400

    METL-48: Fixed favicon

--- metl-ui/src/main/java/org/jumpmind/metl/ui/init/AppShell.java
+++ metl-ui/src/main/java/org/jumpmind/metl/ui/init/AppShell.java
@@ -3,6 +3,7 @@ package org.jumpmind.metl.ui.init;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.theme.Theme;
 
 @Push()
@@ -11,4 +12,8 @@ import com.vaadin.flow.theme.Theme;
 public class AppShell implements AppShellConfigurator {
     private static final long serialVersionUID = 1L;
 
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.addFavIcon("shortcut icon", "/metl/icons/favicon.ico", "32x32");
+    }
 }
